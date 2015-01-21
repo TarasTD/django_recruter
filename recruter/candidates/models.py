cc4c754993db8826a2e202bbc31df9eb1299eaa8
Taras Dmytrus <tarasdmytrus@MacBook-Pro-Taras.local>
2015-01-21 16:54:13 +0200

    initial commit

---
+++ recruter/candidates/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class Technology(models.Model):
+    tech = models.CharField(max_length=50)
+
+
+class Candidate(models.Model):
+    name = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    current_company = models.CharField(max_length=30)
+    date_added = models.DateField(auto_now=True)
+    latest_changes = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField()
+    comment = models.TextField()
+    EXPERIENCE_CHOICES = (
+        ('WJ', 'Weak Junior'),
+        ('J', 'Junior'),
+        ('SJ', 'Strong Junior'),
+        ('WM', 'Weak Middle'),
+        ('M', 'Middle'),
+        ('SM', 'Strong Middle'),
+        ('WS', 'Weak Senior'),
+        ('S', 'Senior'),
+        ('SS', 'Strong Senior'),
+        )
+    experience = models.CharField(max_length=2,
+                                  choices=EXPERIENCE_CHOICES)
+
+    specification = models.ManyToManyField(Technology)
+
